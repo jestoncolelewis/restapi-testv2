8d21655c20cce149a93134b840dadb5b1a488743
Jeston Lewis <jeston.lewis@protonmail.com>
2023-09-21 15:48:39 -0600

    removed comments

--- get_function.py
+++ get_function.py
@@ -1,18 +1,18 @@
 import json
 
 def handler(event, context):
-    # transactionID = event["queryStringParameters"]["transactionId"]
-    # transactionType = event["queryStringParameters"]["type"]
-    # transactionAmount = event["queryStringParameters"]["amount"]
+    transactionID = event["queryStringParameters"]["transactionId"]
+    transactionType = event["queryStringParameters"]["type"]
+    transactionAmount = event["queryStringParameters"]["amount"]
 
-    # print("transactionID=" + transactionID)
-    # print("transactionType=" + transactionType)
-    # print("transactionAmount=" + transactionAmount)
+    print("transactionID=" + transactionID)
+    print("transactionType=" + transactionType)
+    print("transactionAmount=" + transactionAmount)
 
     transactionResponse = {}
-    # transactionResponse["transactionID"] = transactionID
-    # transactionResponse["type"] = transactionType
-    # transactionResponse["amount"] = transactionAmount
+    transactionResponse["transactionID"] = transactionID
+    transactionResponse["type"] = transactionType
+    transactionResponse["amount"] = transactionAmount
     transactionResponse["message"] = "Hello from Lambdaland"
 
     responseObject = {}
